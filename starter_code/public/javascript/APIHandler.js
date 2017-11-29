a82b59a422d0377c69df43d9e75659d3f0895770
Jessica Alin Gonzalez <jessicaalin@Jessicas-MacBook-Pro.local>
2017-11-29 18:15:39 -0500

    sort of done with iteration 1

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -33,15 +33,50 @@ class APIHandler {
     });
   }
 
-  createOneRegister () {
-
+  createOneRegister (nameChar, occupationChar, weaponChar, debtChar) {
+    $.ajax({
+      method: "POST"  ,
+      url: `${this.BASE_URL}/characters`,
+      data: {
+        name: nameChar,
+        occupation: occupationChar,
+        weapon: weaponChar,
+        debt: debtChar
+      }
+    })
+    .then((apiResults) => {
+      console.log("success!");
+      console.log(apiResults);
+    })
+    .catch((err) => {
+      console.log("error!");
+      console.log(err);
+    });
   }
 
   deleteOneRegister () {
 
   }
 
-  updateOneRegister () {
+  updateOneRegister (id, nameEdit, occupationEdit, weaponEdit, debtEdit) {
+    $.ajax({
+      post: "PATCH",
+      url: `${this.BASE_URL}/characters/${id}`,
+      data: {
+        name: nameEdit,
+        occupation: occupationEdit,
+        weapon: weaponEdit,
+        debt: debtEdit
+      }
+    })
+    .then((apiResults) => {
+      console.log("success!");
+      console.log(apiResults);
+    })
+    .catch((err) => {
+      console.log("error!");
+      console.log(err);
+    });
 
   }
 
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -12,18 +12,32 @@ $(document).ready( () => {
     charactersAPI.getOneRegister(idNumber);
   });
 
-  $('#new-character-form').on('submit', (e) => {
-
+  $('#new-character-form').submit(() => {
+    event.preventDefault();
+    console.log("click!");
+    const charId = $("#edit-id").val();
+    const nameChar = $("#name").val();
+    const occupationChar = $("#occupation").val();
+    const weaponChar = $("#weapon").val();
+    const debtChar = $("#debt").val();
+    charactersAPI.createOneRegister(nameChar, occupationChar, weaponChar, debtChar);
   });
-  
+
   $('#delete-one').click(() => {
     console.log("click!");
     charactersAPI.deleteOneRegister();
 
   });
 
-  $('#edit-character-form').on('submit', (e) => {
-
+  $('#edit-character-form').submit(() => {
+    event.preventDefault();
+    console.log("click!");
+    const id = $("#edit-id").val();
+    const nameEdit = $("#edit-name").val();
+    const occupationEdit = $("#edit-occupation").val();
+    const weaponEdit = $("#edit-weapon").val();
+    const debtEdit = $("#edit-debt").val();
+    charactersAPI.updateOneRegister(id, nameEdit, occupationEdit, weaponEdit, debtEdit);
   });
 
 
